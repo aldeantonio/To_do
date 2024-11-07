0ad563e7c2eedb082fd1893dfe41a337c7e7f37c
Alde Antonio <aldeantonio8@gmail.com>
2024-11-07 14:30:13 +0200

    Add task Styles

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,31 +1,36 @@
-import React from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-
       {/*Today's tasks*/}
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
 
         <View style={styles.items}>
           {/*This is where the tasks will do! */}
-
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
         </View>
       </View>
-
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor: '#fff',
+    flex: 1,
+    backgroundColor: "#ffe",
   },
-  tasksWrapper: {},
-  sectionTitle: {},
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
   items: {},
 });
